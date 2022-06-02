97288c775d5ffb0b8ea65ded54aef577a0cb7d3a
grointape <jason111liang@gmail.com>
2022-06-02 18:39:31 +1000

    javacript for submit button

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+
+// code for enqiury popup
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+
+
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
+
+// code for stuff
